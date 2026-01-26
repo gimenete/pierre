082dedadab2e7ca4c7130092916bed77367a9bcb
Amadeus Demarzi <amadeusdemarzi@gmail.com>
2026-01-26 00:59:35 -0800

    Update renderFileWithHighlighter to use new iterator and prep for windowing apis

--- packages/diffs/src/worker/WorkerPoolManager.ts
+++ packages/diffs/src/worker/WorkerPoolManager.ts
@@ -461,7 +461,12 @@ export class WorkerPoolManager {
     this.submitTask(instance, { type: 'file', file });
   }
 
-  getPlainFileAST(file: FileContents): ThemedFileResult | undefined {
+  getPlainFileAST(
+    file: FileContents,
+    startingLine: number,
+    totalLines: number,
+    lines?: string[]
+  ): ThemedFileResult | undefined {
     if (this.highlighter == null) {
       void this.initialize();
       return undefined;
@@ -470,7 +475,7 @@ export class WorkerPoolManager {
       file,
       this.highlighter,
       this.renderOptions,
-      true
+      { forcePlainText: true, startingLine, totalLines, lines }
     );
   }
 
